c00912af5c86dce669abe8bd76506a319eb706dd
Danielmagox <daniel1305972@gmail.com>
2020-11-25 19:13:09 +0000

    ADD Norris Joke

---
+++ src/components/NorrisJoke.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    Norris Joke
+  </div>
+</template>
+
+<script>
+const URL = "https://api.chucknorris.io/jokes/random";
+export default {
+  name: "Norris Joke",
+  methods: {
+    fetchApi() {}
+  },
+  data() {
+    return {
+      icon: "",
+      text: "",
+      date: ""
+    };
+  }
+};
+</script>
+
+<style></style>
