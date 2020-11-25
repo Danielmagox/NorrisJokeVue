9878d961c29fb53c5a9b0d4eeff27f0bca91ebed
Danielmagox <daniel1305972@gmail.com>
2020-11-25 19:20:44 +0000

    ADD methods in Norris Jode, and .json in assets

--- src/components/NorrisJoke.vue
+++ src/components/NorrisJoke.vue
@@ -9,7 +9,15 @@ const URL = "https://api.chucknorris.io/jokes/random";
 export default {
   name: "Norris Joke",
   methods: {
-    fetchApi() {}
+    fetchApi() {
+      fetch(URL)
+        .then(res => res.json())
+        .then(data => {
+          this.icon = data.icon_url;
+          this.text = data.value;
+          this.date = data.updated_at;
+        });
+    }
   },
   data() {
     return {
